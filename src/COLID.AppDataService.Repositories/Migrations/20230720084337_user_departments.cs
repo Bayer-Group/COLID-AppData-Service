644003f60b3068b89f0744e769bc98f355b05230
Kanwalmeet Singh <kanwalmeet.singhkochar.ext@bayer.com>
2024-01-05 16:39:03 +0100

    added migrations

---
+++ src/COLID.AppDataService.Repositories/Migrations/20230720084337_user_departments.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repositories.Migrations
+{
+    public partial class userdepartments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "department",
+                table: "users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "department",
+                table: "users");
+        }
+    }
+}
